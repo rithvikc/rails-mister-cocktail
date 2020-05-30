a61932b79c665901ba1e24b5983462ee4af1e701
Rithvik Chand <rithvikchand@gmail.com>
2020-05-30 14:37:11 +1000

    deployment

---
+++ db/migrate/20200530033511_add_dose_to_cocktails.rb
@@ -0,0 +1,7 @@
+class AddDoseToCocktails < ActiveRecord::Migration[6.0]
+  def change
+    # add_reference :doses, :cocktail, foreign_key: true
+    # add_reference :doses, :ingredient, foreign_key: true
+
+  end
+end
